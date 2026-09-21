235089ffbae11863a6be0c998b33151509482b68
Claudio <seuemail@gmail.com>
2026-09-21 20:51:00 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import { PokemonCard } from "./pokemon/PokemonCard.jsx";
+import { Header } from "./Header";
+import { PokemonCard } from "./pokemon/PokemonCard";
 
-function App() {
+
+export function App() {
+  // cada pokémon agora é um objeto só, com todos os dados dele dentro
+  const charmander = {
+    id: "#004",
+    name: "Charmander",
+    type: "Fogo",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+  };
+
+  const squirtle = {
+    id: "#007",
+    name: "Squirtle",
+    type: "Agua",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+  };
+
+  const bulbasaur = {
+    id: "#001",
+    name: "Bulbasaur",
+    type: "Planta",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  };
+
   return (
-    <div className="pokemon-list">
-      <PokemonCard
-        id="004"
-        name="Charmander"
-        image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-        type="fire"
-        typeLabel="Fogo"
-      />
-      <PokemonCard
-        id="001"
-        name="Bulbasaur"
-        image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        type="grass"
-        typeLabel="Planta"
-      />
-      <PokemonCard
-        id="007"
-        name="Squirtle"
-        image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-        type="water"
-        typeLabel="Água"
-      />
-    </div>
+    <>
+      <Header />
+
+      <div className="cards-grid">
+        {/* o mesmo componente PokemonCard, reaproveitado 3 vezes com dados diferentes */}
+        <PokemonCard pokemon={charmander} />
+        <PokemonCard pokemon={squirtle} />
+        <PokemonCard pokemon={bulbasaur} />
+      </div>
+    </>
   );
 }
 
--- src/pokemon/PokemonCard.jsx
+++ src/pokemon/PokemonCard.jsx
@@ -1,19 +1,24 @@
 import "./PokemonCard.css";
 
-export function PokemonCard({ id, name, image, type, typeLabel }) {
+// agora o componente recebe um único prop chamado "pokemon" (um objeto)
+// e desestrutura os campos que precisa logo na primeira linha
+export function PokemonCard({ pokemon }) {
+  const { id, name, type, image } = pokemon;
+
   return (
     <article className="pokemon-card">
       <header className="card-header">
-        <span className="pokemon-id">#{id}</span>
+        <span className="pokemon-id">{id}</span>
         <h2 className="pokemon-name">{name}</h2>
       </header>
 
       <figure className="pokemon-image-container">
-        <img src={image} alt={`Ilustração do ${name}`} />
+        <img src={image} alt={`Foto do ${name}`} />
       </figure>
 
       <ul className="pokemon-types">
-        <li className={`type-badge type-${type}`}>{typeLabel}</li>
+        {/* classe dinâmica: muda de acordo com o tipo do pokémon */}
+        <li className={`type-badge type-${type.toLowerCase()}`}>{type}</li>
       </ul>
     </article>
   );
